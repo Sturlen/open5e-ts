d0167580d614277347cf19b98e8429b7b0ab77a0
sturlen <43607012+Sturlen@users.noreply.github.com>
2024-03-01 00:05:04 +0100

    fix: load fixtures without destroying TS and simplify

--- monsters.test.ts
+++ monsters.test.ts
@@ -1,20 +1,28 @@
 import { expect, it, beforeAll, afterAll, describe } from "vitest"
 import Open5eAPI from "./src"
 import fetchMock from "fetch-mock"
+import fs from "fs"
 
-import aboleth from "./fixtures/aboleth.json"
-import cc from "./fixtures/cc-limit-50.json"
-import tob2andtob3 from "./fixtures/tob-and-tob3.json"
-import findOne from "./fixtures/findOne.json"
-import findTwenty from "./fixtures/find20.json"
-import findFifty from "./fixtures/find50.json"
-import findDragons from "./fixtures/find-dragons.json"
-import find18CR from "./fixtures/find-cr-one-eigth.json"
-import empty from "./fixtures/empty.json"
+const aboleth = JSON.parse(fs.readFileSync("./fixtures/aboleth.json", "utf-8"))
+const cc = JSON.parse(fs.readFileSync("./fixtures/cc-limit-50.json", "utf-8"))
+const tob2andtob3 = JSON.parse(
+    fs.readFileSync("./fixtures/tob-and-tob3.json", "utf-8")
+)
+const findOne = JSON.parse(fs.readFileSync("./fixtures/findOne.json", "utf-8"))
+const findTwenty = fs.readFileSync("./fixtures/find20.json", "utf-8")
+const findFifty = JSON.parse(fs.readFileSync("./fixtures/find50.json", "utf-8"))
+const findDragons = JSON.parse(
+    fs.readFileSync("./fixtures/find-dragons.json", "utf-8")
+)
+const find18CR = JSON.parse(
+    fs.readFileSync("./fixtures/find-cr-one-eigth.json", "utf-8")
+)
+const empty = JSON.parse(fs.readFileSync("./fixtures/empty.json", "utf-8"))
 
 const MONSTER_PATH = "/monsters/"
 const ENDPOINT = "https://api.example.com"
 const MONSTER_ENDPOINT = `${ENDPOINT}${MONSTER_PATH}`
+const api = Open5eAPI(ENDPOINT)
 
 beforeAll(() => {
     fetchMock
@@ -48,21 +56,18 @@ afterAll(() => {
 
 describe("findOne", () => {
     it("Gets a monster by it's slug", async () => {
-        const api = Open5eAPI(ENDPOINT)
         const mon = await api.monsters.findOne("aboleth")
 
         expect(mon.name).toBe("Aboleth")
     })
 
     it("Throws if a monster is not found", async () => {
-        const api = Open5eAPI(ENDPOINT)
         expect(() => api.monsters.findOne("not-a-monster")).rejects.toThrow(
             "Monster with slug 'not-a-monster' was not found."
         )
     })
 
     it("Throws if slug is empty", async () => {
-        const api = Open5eAPI(ENDPOINT)
         expect(() => api.monsters.findOne("")).rejects.toThrow(
             "Slug is required."
         )
@@ -71,14 +76,12 @@ describe("findOne", () => {
 
 describe("findMany", () => {
     it("Fetches 50 monsters by default", async () => {
-        const api = Open5eAPI(ENDPOINT)
         const mons = await api.monsters.findMany()
 
         expect(mons.length).toBe(50)
     })
 
     it("If no monsters are found, return an empty list", async () => {
-        const api = Open5eAPI(ENDPOINT)
         const mons = await api.monsters.findMany({
             document__slug: "not-a-document",
         })
@@ -87,7 +90,6 @@ describe("findMany", () => {
     })
 
     it("Can filter by a document slug", async () => {
-        const api = Open5eAPI(ENDPOINT)
         const mons = await api.monsters.findMany({ document__slug: "cc" })
 
         expect(mons.length).toBeGreaterThan(0)
@@ -97,12 +99,11 @@ describe("findMany", () => {
     })
 
     it("Can filter by multiple document slugs", async () => {
-        const api = Open5eAPI(ENDPOINT)
         const mons = await api.monsters.findMany({
             limit: 800,
             document__slug: ["tob2", "tob3"],
         })
-        expect(mons.length).toBeGreaterThan(0)
+
         const number_of_monsters = mons.length
         const number_of_a_monsters = mons.filter(
             (mon) => mon.document__slug === "tob2"
@@ -111,16 +112,15 @@ describe("findMany", () => {
             (mon) => mon.document__slug === "tob3"
         ).length
 
+        expect(mons.length).toBeGreaterThan(0)
         expect(number_of_a_monsters).toBeGreaterThan(0)
         expect(number_of_b_monsters).toBeGreaterThan(0)
-
         expect(number_of_monsters).toBe(
             number_of_a_monsters + number_of_b_monsters
         )
     })
 
     it("Can limit how many monsters are returned", async () => {
-        const api = Open5eAPI(ENDPOINT)
         const one = await api.monsters.findMany({ limit: 1 })
         const twenty = await api.monsters.findMany({ limit: 20 })
 
@@ -129,14 +129,12 @@ describe("findMany", () => {
     })
 
     it("Can filter by a string. This can be in the name as well as other fields", async () => {
-        const api = Open5eAPI(ENDPOINT)
         const monsters = await api.monsters.findMany({ search: "dragon" })
 
         expect(monsters[0].name).toBe("Adult Black Dragon")
     })
 
     it("Can filter by challenge rating.", async () => {
-        const api = Open5eAPI(ENDPOINT)
         const one_eight_cr = await api.monsters.findMany({
             limit: 100,
             challenge_rating: 1 / 8,
